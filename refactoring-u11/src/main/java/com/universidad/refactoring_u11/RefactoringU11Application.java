576c893df81615634a7bd5f177eaed513f14f607
moises <moisescardenas807@gmail.com>
2026-05-17 14:47:18 -0500

    feat:Inicializar post1 con spring boot

---
+++ refactoring-u11/src/main/java/com/universidad/refactoring_u11/RefactoringU11Application.java
@@ -0,0 +1,13 @@
+package com.universidad.refactoring_u11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringU11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringU11Application.class, args);
+	}
+
+}
---
+++ refactoring-u11/src/test/java/com/universidad/refactoring_u11/RefactoringU11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.refactoring_u11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringU11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
